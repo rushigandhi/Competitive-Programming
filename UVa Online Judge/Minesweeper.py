5e6484bb6d5fd14a9ffb3c222bc6b676065b8a24
rushigandhi <rushi.gandhi@hotmail.com>
2017-08-06 22:48:46 -0400

    Revised Minesweeper Solution

--- UVa Online Judge/Minesweeper.py
+++ UVa Online Judge/Minesweeper.py
@@ -2,22 +2,30 @@ inputLine = input()
 inputLine.split()
 length = int(inputLine[0])
 wid = int(inputLine[2])
-grid = [[0 for j in range (length)] for i in range (wid)]
+grid = [[0 for j in range (length + 2)] for i in range (wid + 2)]
 mineList = []
 for x in range(length):
-        tempList = input()
-        tempList.split()
+        tempList = list(input())
         for i in range(wid):
             if tempList[i] == '*':
                 coordinates = str(i) + str(x)
                 mineList.append(coordinates)
-        grid[x] = list(tempList)
+        grid[x+1] = [""]+tempList+[""]
 
-
-for y in range(length):
-    for x in range(wid):
+print(grid)
+for y in range(1, length + 1):
+    for x in range(1, wid + 1):
         counter = 0
         if grid[y][x] == '.':
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    neighX = x + i
+                    neighY = y + j
+                    if grid[neighY][neighX] == "*":
+                        counter+=1
+            grid[y][x] = str(counter)
+
+            '''
             for i in range(len(mineList)):
                 mineCoord = int(mineList[i])
                 mineX = (mineCoord//10)%10
@@ -30,10 +38,10 @@ for y in range(length):
                     counter += 1
                 # print("counter", counter)
             grid[y][x] = str(counter)
-            # print("counter", counter)
+            # print("counter", counter)'''
 
-for y in range(length):
-        line = ''.join(str(e) for e in grid[y])
+for y in range(1, length + 1):
+        line = ''.join(str(e) for e in grid[y][1:] )
         print(line)
 
 '''grid = list(filter(None, grid))
