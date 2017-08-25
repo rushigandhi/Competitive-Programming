7a115e56f03471377bae449555cbd499c72db180
rushigandhi <rushi.gandhi@hotmail.com>
2017-08-24 23:44:13 -0400

    (wip): Frequency Sorter for Task SORT

--- COCI/Task_SORT.py
+++ COCI/Task_SORT.py
@@ -1,4 +1,20 @@
-inputList = [int(e) for e in input().sort()]
+inputList = [int(e) for e in input().split()]
 N = inputList[0]
 C = inputList[1]
-reading = input().split()
+numList = list()
+freqList = list()
+message = [int(e) for e in input().split()]
+for i in range(N):
+    if message[i] in numList:
+        iVal = numList.index(message[i])
+        freqList[iVal] += 1
+    else:
+        numList.append(message[i])
+        freqList.append(1)
+
+
+print(numList, freqList)
+
+'''
+9 77
+11 33 11 77 54 11 25 25 33'''
