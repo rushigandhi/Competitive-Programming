65b5ec409abd4336db441e0e40e6966b52723c02
rushigandhi <rushi.gandhi@hotmail.com>
2017-08-25 23:26:37 -0400

    (wip): COCI Task SORT

--- COCI/Task_SORT.py
+++ COCI/Task_SORT.py
@@ -11,10 +11,36 @@ for i in range(N):
     else:
         numList.append(message[i])
         freqList.append(1)
+print(numList,freqList)
+for i in range(len(freqList)):
+    for x in range(len(freqList) - 1):
+        if freqList[x + 1] < freqList[x]:
+            tempFreq = freqList[x + 1]
+            tempNum = numList[x + 1]
+            freqList[x + 1] = freqList[x]
+            numList[x + 1] = numList[x]
+            freqList[x] = tempFreq
+            numList[x] = tempNum
+        if message.index(numList[x + 1]) < message.index(numList[x]) and freqList[x + 1] == freqList[x]:
+            tempFreq = freqList[x + 1]
+            tempNum = numList[x + 1]
+            freqList[x + 1] = freqList[x]
+            numList[x + 1] = numList[x]
+            freqList[x] = tempFreq
+            numList[x] = tempNum
+        print(numList, freqList)
+
+revNum = list(reversed(numList))
+revFreq = list(reversed(freqList))
+
+for i in range(len(revNum)):
+    for x in range(revFreq[i]):
+        print(revNum[i])
 
 
-print(numList, freqList)
 
 '''
 9 77
-11 33 11 77 54 11 25 25 33'''
+11 33 11 77 54 11 25 25 33
+
+or freqList[x + 1] == freqList[x]'''
